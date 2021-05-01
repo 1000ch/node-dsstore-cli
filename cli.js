29078e00bcec12bedf77e7ba3079f2e79edc7deb
1000ch <shogosensui@gmail.com>
2021-05-01 15:09:24 +0900

    refactor: use native ESM

--- cli.js
+++ cli.js
@@ -1,46 +1,49 @@
 #!/usr/bin/env node
-const fs = require('fs');
-const path = require('path');
-const minimist = require('minimist');
-const ora = require('ora');
-const dsstore = require('dsstore');
-
-const argv = minimist(process.argv.slice(2), {
-  alias: {
-    v: 'version',
-    h: 'help',
-    s: 'silent'
-  }
-});
-
-if (argv.v || argv.version) {
-  console.log(require('./package').version);
-  return;
-}
-
-if (argv.h || argv.help) {
-  fs.createReadStream(`${__dirname}/usage.txt`)
-    .pipe(process.stdout)
-    .on('close', () => process.exit(1));
-  return;
-}
-
-const cwd = process.cwd();
-const args = argv._.length ? argv._.map(arg => path.resolve(cwd, arg)) : [cwd];
+import fs from 'fs/promises';
+import path from 'path';
+import minimist from 'minimist';
+import ora from 'ora';
+import dsstore from 'dsstore';
 
 (async () => {
   try {
+    const argv = minimist(process.argv.slice(2), {
+      alias: {
+        v: 'version',
+        h: 'help',
+        s: 'silent'
+      }
+    });
+
+    if (argv.v || argv.version) {
+      const data = await fs.readFile(new URL('package.json', import.meta.url));
+      const pkg = JSON.parse(data);
+      console.log(pkg.version);
+    }
+
+    if (argv.h || argv.help) {
+      const data = await fs.readFile(new URL('usage.txt', import.meta.url));
+      console.log(data.toString());
+      process.exit(0);
+    }
+
+    const cwd = process.cwd();
+    const args = argv._.length === 0 ? [cwd] : argv._.map(arg => path.resolve(cwd, arg));
+
     if (argv.s || argv.silent) {
       await dsstore(args);
     } else {
-      const spinner = ora('Searching .DS_Store').start();
+      const spinner = ora('Searching .DS_Store');
+      spinner.start();
 
       await dsstore(args, path => {
         spinner.text = `Deleted ${path}`;
         spinner.succeed();
       });
+
+      process.exit(0);
     }
   } catch (error) {
-    console.error(error)
+    console.error(error);
   }
 })();
